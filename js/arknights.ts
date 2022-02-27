96cf1f6b250d2e771c7c4b91ce14f69b0baece5a
what-is-me <2454787428@qq.com>
2022-02-27 23:25:10 +0800

    Site updated: 2022-02-27 23:25:10

--- js/arknights.ts
+++ js/arknights.ts
@@ -11,7 +11,6 @@ class dust {
   public radiusY: number = Math.random() * 3
   public rotation: number = Math.PI *  Math.floor(Math.random() * 2)
 }
-
 class canvasDust {
   private readonly canvas: HTMLCanvasElement | undefined
   private readonly ctx: CanvasRenderingContext2D | null | undefined
@@ -19,7 +18,6 @@ class canvasDust {
   public height: number = 300
   private dustQuantity: number = 50
   public dustArr:Array<dust> = []
-
   constructor(canvasID: string) {
     const canvas: HTMLCanvasElement =
       document.getElementById(canvasID) as HTMLCanvasElement
@@ -32,7 +30,6 @@ class canvasDust {
       throw new Error('canvasID 无效')
     }
   }
-
   private build() {
     this.resize()
     if (this.ctx) {
@@ -47,7 +44,6 @@ class canvasDust {
       }, 40)
     }
   }
-
   private play() {
     const dustArr = this.dustArr
     this.ctx?.clearRect(0,0, this.width, this.height)
@@ -65,7 +61,6 @@ class canvasDust {
       }
     }
   }
-
   private buildDust(x: number, y: number, dust: dust) {
     const ctx = this.ctx
     if (x) dust.x = x
@@ -82,7 +77,6 @@ class canvasDust {
       ctx.fill()
     }
   }
-
   private resize() {
     const canvas = this.canvas
     const width = window.innerWidth
@@ -95,7 +89,6 @@ class canvasDust {
       canvas.height = height
     }
   }
-
   private static getPoint(number: number = 1): Array<[number, number]> {
     let point: Array<[number, number]> = []
     for (let i: number = 0; i<number; i++) {
@@ -106,7 +99,6 @@ class canvasDust {
     return point
   }
 }
-
 class indexs {
   headerLink: HTMLCollection
   tocLink: HTMLCollection
@@ -115,7 +107,6 @@ class indexs {
   totop: HTMLElement = document.getElementById('to-top')
   scrollID: number = null
   scrolling: number = 0
-
   private setItem(item: Element) {
     item.classList.add('active')
     let $parent = item.parentElement, brother = $parent.children
@@ -132,7 +123,6 @@ class indexs {
       }
     }
   }
-
   private reset() {
     let tocs = document.getElementsByClassName('active')
     let tocTree = document.getElementsByClassName('has-active')
@@ -145,7 +135,6 @@ class indexs {
       item.classList.remove('has-active')
     }
   }
-
   private modifyIndex() {
     for (let i = 0; i < this.headerLink.length; i++) {
       const link = this.headerLink.item(i) as HTMLElement
@@ -163,24 +152,25 @@ class indexs {
     }
     this.index = []
   }
-
   private scrolltop() {
     window.scroll({top: 0,left: 0,behavior: 'smooth'});
     document.getElementById('to-top').style.opacity = '0';
+    setTimeout(()=> this.totop.style.display = 'none', 300);
   }
 
   constructor() {
-    this.headerLink = document.getElementsByClassName('headerlink')
     this.tocLink = document.getElementsByClassName('toc-link')
-    this.postContent = document.getElementById('post-content')
-    const totop = document.getElementById('to-top')
-    if (totop != null)
-      totop.style.opacity = '0';
     if (this.tocLink.length > 0) {
       this.setItem(this.tocLink.item(0))
-      document.addEventListener('scroll', ()=>{
+    }
+    document.addEventListener('scroll', ()=>{
+      this.tocLink = document.getElementsByClassName('toc-link')
+      if (this.tocLink.length > 0) {
+        this.headerLink = document.getElementsByClassName('headerlink')
+        this.postContent = document.getElementById('post-content')
+        const totop = document.getElementById('to-top')
         ++this.scrolling
-        if (this.scrollID == null) {
+        if (this.scrollID == null && this.tocLink.length > 0) {
           this.scrollID = setInterval(this.modifyIndex.bind(this), 50)
         }
         setTimeout(()=>{
@@ -190,20 +180,20 @@ class indexs {
             const totop = document.getElementById('to-top')
             if (this.totop !== null
               && document.getElementById('post-title').getBoundingClientRect().top < -200) {
-              totop.style.opacity = '1';
+              totop.style.display = '';
+              setTimeout(()=> totop.style.opacity = '1', 300);
             } else {
               totop.style.opacity = '0';
+              setTimeout(()=> totop.style.display = 'none', 300);
             }
           }
         }, 200);
-      }, {passive: true})
-    }
+      }
+    }, {passive: true})
   }
 }
 
 class codes {
-
-
   private reverse(item: Element, s0: string, s1: string) {
     const block = item.parentElement
     if (block.classList.contains(s0)){
@@ -214,18 +204,15 @@ class codes {
       block.classList.add(s0)
     }
   }
-
   private doAsMermaid(item: Element) {
     let Amermaid = item.getElementsByClassName('mermaid').item(0) as HTMLElement
     item.outerHTML = '<div class="highlight mermaid">' + Amermaid.innerText + '</div>'
   }
-
   private resetName(str: string): string {
     if (str == 'plaintext') return 'TEXT'
     if (str == 'cs') return 'C#'
     return str.toUpperCase()
   }
-
   private doAsCode(item: Element): void {
     const codeType = item.classList[1], lineCount= item.getElementsByClassName('gutter').item(0).children[0].childElementCount >> 1
     item.classList.add(lineCount < 16 ? 'open' : 'fold')
@@ -251,7 +238,6 @@ class codes {
       }
     })
   }
-
   private findCode(): void{
     let codeBlocks = document.getElementsByClassName('highlight')
     for (let i = 0; i < codeBlocks.length; i++) {
@@ -264,9 +250,7 @@ class codes {
     }
   }
 
-  constructor() {
-    this.findCode()
-  }
+  constructor() {}
 }
 
 class cursors {
@@ -279,7 +263,6 @@ class cursors {
   ishead: boolean = true
   moveEventID: number = null
   fadeEventID: number = null
-
   private move(): void {
     if (this.now !== undefined) {
       let SX = this.outer.left, SY = this.outer.top
@@ -305,7 +288,6 @@ class cursors {
       }
     }
   }
-
   private reset(mouse): void {
     if (this.moveEventID === null) {
       this.moveEventID = window.setInterval(this.move.bind(this), 1)
@@ -317,7 +299,6 @@ class cursors {
       this.outer.top = String(this.now.y) + 'px'
     }
   }
-
   private fadeOut(): void {
     if (this.opacity > 0) {
       let delta = this.opacity * 0.11
@@ -331,7 +312,6 @@ class cursors {
       this.fadeEventID = null
     }
   }
-
   private Aeffect(mouse): void {
     if (this.fadeEventID === null) {
       this.fadeEventID = window.setInterval(this.fadeOut.bind(this), 10)
@@ -343,19 +323,16 @@ class cursors {
       this.opacity = 1
     }
   }
-
   private hold(): void {
     this.outer.height = '24px'
     this.outer.width = '24px'
     this.outer.background = "rgba(255, 255, 255, 0.5)"
   }
-
   private relax(): void {
     this.outer.height = '36px'
     this.outer.width = '36px'
     this.outer.background = "unset"
   }
-
   private pushHolder(items: HTMLCollection): void {
     for (let i = 0; i < items.length; i++) {
       const item = items.item(i)
@@ -363,7 +340,6 @@ class cursors {
       item.addEventListener('mouseout',()=> this.relax(), {passive: true})
     }
   }
-
   private pushHolders(): void {
     this.pushHolder(document.getElementsByTagName('a'));
     this.pushHolder(document.getElementsByTagName('input'));
@@ -372,7 +348,6 @@ class cursors {
     this.pushHolder(document.getElementsByClassName('gt-user-inner'));
     this.pushHolder(document.getElementsByClassName('gt-header-textarea'));
   }
-
   constructor() {
     document.querySelector('header').addEventListener('mouseenter', ()=> this.ishead = true, {passive: true});
     document.querySelector('header').addEventListener('mouseout', ()=> this.ishead = false, {passive: true});
@@ -383,7 +358,6 @@ class cursors {
     observer.observe(document, {childList: true, subtree: true});
   }
 }
-
 let index = new indexs()
 let code = new codes()
 let cursor = new cursors()
